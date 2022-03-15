05d572241aa7d88a6b4c237ccb4ad56fc5f0a4a3
3n3a <enea_kraehenbuehl@sluz.ch>
2022-03-15 12:56:08 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-knvbaz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-knvbaz)
---
+++ index.js
@@ -0,0 +1,31 @@
+// url:port/#key=value&key2=value2
+class ClientParameters {
+  params = [];
+  constructor() {
+    this.params = this.getParams();
+  }
+
+  /**
+   * getParams
+   * Returns key:value array of params provided by hash
+   * @returns array
+   */
+  getParams() {
+    let path = location.hash.split('#')[1];
+    if (path == '') return Null;
+    let paramsEncoded = path.split('&');
+    let params = [];
+    for (const param of paramsEncoded) {
+      const [key, value] = param.split('=');
+      params.push({ key: key, value: value });
+    }
+
+    return params;
+  }
+}
+
+location.hash = '#your=mome&is=value';
+let c = new ClientParameters();
+for(const param of c.getParams()) {
+  document.body.insertAdjacentHTML('beforeend', `<p>${param.key} = ${param.value}</p>`)
+}
