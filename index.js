34a1d1019b17a0145cb8f05a8ec8208420768b60
3n3a <enea_kraehenbuehl@sluz.ch>
2022-03-15 13:54:22 +0100

    add option to create own url

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ class ClientParameters {
    * @returns array
    */
   getParams() {
-    let path = location.hash.split('#')[1];
+    let path = location.hash.split('#')[1] ?? '';
     if (path == '') return null;
     let paramsEncoded = path.split('&');
     let params = {};
@@ -25,7 +25,7 @@ class ClientParameters {
         value = atob(value);
       }
 
-      params[decodeURI(key)] = value
+      params[decodeURI(key)] = value;
     }
 
     return params;
@@ -41,7 +41,67 @@ function outputAllParams(params) {
   }
 }
 
-location.hash = '#content=mome&is=value';
+function renderContent(body, head = null) {
+  if (head) {
+    document.head.innerHTML = head;
+  }
+  document.body.innerHTML = body;
+}
+
+// urlsafe b64
+function encode(buffer) {
+  return buffer
+    .toString('base64')
+    .replace(/\+/g, '-') // Convert '+' to '-'
+    .replace(/\//g, '_') // Convert '/' to '_'
+    .replace(/=+$/, ''); // Remove ending '='
+}
+
+//location.hash = '#body=mome&head=value';
+// example: #body=b64_PGltZyBzcmM9Imh0dHBzOi8vc291cmNlLnVuc3BsYXNoLmNvbS9yYW5kb20iLz4&head=b64_PGxpbmsgcmVsPSJzdHlsZXNoZWV0IiBocmVmPSJodHRwczovL2Nkbi5qc2RlbGl2ci5uZXQvbnBtL3NwY3NzIj4
 let c = new ClientParameters();
 let params = c.getParams();
-outputAllParams(params);
+let hasParams = params != null;
+if (hasParams && params.hasOwnProperty('body')) {
+  let head = params.hasOwnProperty('head') ? params.head : null;
+  renderContent(params.body, head);
+} else {
+  document.body.insertAdjacentHTML(
+    'afterbegin',
+    `<h2>Da input u provided</h2>`
+  );
+  hasParams ? outputAllParams(params) : '';
+  document.body.insertAdjacentHTML(
+    'beforeend',
+    `
+    <h2>Create ur special url</h2>
+    <label for="head">Paste the Head (optional)</label><br>
+    <textarea id="head"></textarea><br><br>
+
+    <label for="body">Paste the Body</label><br>
+    <textarea id="body" required></textarea><br>
+
+    <button id="gen">Generate</button>
+    `
+  );
+
+  document.getElementById('gen').addEventListener('click', function () {
+    let head = document.getElementById('head').value;
+    let body = document.getElementById('body').value;
+    if (body) {
+      let url =
+        location.origin +
+        '/#' +
+        'body=b64_' +
+        encode(body) +
+        (head ? '&head=b64_' + head : '');
+
+      document.body.insertAdjacentHTML(
+        'beforeend',
+        '<br><a href ="' + url + '">' + url + '</a>'
+      );
+    } else {
+      console.log('no input for gen');
+    }
+  });
+}
